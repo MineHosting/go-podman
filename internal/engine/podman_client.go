292543a9476b4d9caaec43c9158fab0a59224414
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-05-02 02:07:24 -0300

    feat: add a definition of a podman client with some internal functions

---
+++ internal/engine/podman_client.go
@@ -0,0 +1,82 @@
+// go-podman/engine/podman_client.go
+package engine
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+
+	"github.com/MineHosting/go-podman/internal/network"
+	"github.com/MineHosting/go-podman/internal/socket"
+)
+
+type PodmanClient struct {
+	ApiVersion string
+
+	socketType   socket.SocketPath
+	socketClient socket.SocketClient
+}
+
+func NewPodmanClient(st socket.SocketPath) *PodmanClient {
+	return &PodmanClient{
+		ApiVersion: "v1.0.0",
+		socketType: st,
+		socketClient: socket.SocketClient{
+			Socket:            st,
+			Serializer:        &network.RealPayloadSerializer{},
+			RequestBuilder:    &network.RealHTTPRequestBuilder{},
+			ResponseReader:    &network.RealResponseReader{},
+			ResponseValidator: &network.RealResponseValidator{},
+			NetworkTransport:  &network.RealTransportCreator{},
+		},
+	}
+}
+
+func (pd *PodmanClient) ChangeApiVersion() error {
+	const versionEndpoint = "libpod/version"
+
+	resp, err := pd.Send("GET", versionEndpoint, nil)
+	if err != nil {
+		return fmt.Errorf("[Engine]: failed to fetch version info: %w", err)
+	}
+
+	var parsed struct {
+		Components []struct {
+			Name    string `json:"Name"`
+			Details struct {
+				APIVersion string `json:"APIVersion"`
+			} `json:"Details"`
+		} `json:"Components"`
+	}
+
+	if err := json.Unmarshal(resp, &parsed); err != nil {
+		return fmt.Errorf("[Engine]: failed to parse version components: %w", err)
+	}
+
+	for _, comp := range parsed.Components {
+		if comp.Name == "Podman Engine" {
+			if comp.Details.APIVersion == "" {
+				return fmt.Errorf("[Engine]: Podman Engine found but missing APIVersion")
+			}
+			pd.ApiVersion = fmt.Sprintf("v%s", comp.Details.APIVersion)
+			return nil
+		}
+	}
+
+	return fmt.Errorf("[Engine]: Podman Engine component not found in version response")
+}
+
+func (pd *PodmanClient) Send(method, endpoint string, body any) ([]byte, error) {
+	var serializedBody io.Reader
+	var err error
+	if body != nil {
+		serializedBody, err = pd.socketClient.Serializer.SerializePayload(body)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	url := fmt.Sprintf("%s%s", pd.ApiVersion, endpoint)
+
+	return pd.socketClient.Send(method, url, serializedBody)
+}
