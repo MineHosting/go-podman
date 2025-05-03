0154323fdc8da7b3ae7be1dfe4f39c27381f6c6d
KitsuneSemCalda <arthuramagalhaes1312@gmail.com>
2025-05-03 14:12:48 -0300

    feat: Create a dynamic rootless path builder using a user UID to generate a new path

--- internal/socket/socket_types.go
+++ internal/socket/socket_types.go
@@ -1,9 +1,25 @@
 // go-podman/internal/socket/socket_types.go
 package socket
 
+import (
+	"fmt"
+	"os/user"
+)
+
 type SocketPath string
 
-const (
+var (
 	Rootfull SocketPath = "/run/podman/io.podman/podman.sock"
-	Rootless SocketPath = "/run/user/1000/podman/podman.sock"
+	Rootless SocketPath
 )
+
+func GetRootlessPath() error {
+	CollectUser, err := user.Current()
+	if err != nil {
+		return fmt.Errorf("[GetRootlessPath]: Can't get info about user: %w", err)
+	}
+
+	Rootless = SocketPath(fmt.Sprintf("/run/user/%s/podman/podman.sock", CollectUser.Uid))
+
+	return nil
+}
